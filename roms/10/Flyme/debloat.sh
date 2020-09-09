4b7f5c3bf8790a569d507eed8c30797b0260be10
Luke Jackson <63535895+lukejacksonbristot@users.noreply.github.com>
2020-09-09 18:46:53 +0800

    Update debloat.sh

--- roms/10/Flyme/debloat.sh
+++ roms/10/Flyme/debloat.sh
@@ -4,8 +4,6 @@ systempath=$1
 thispath=`cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd`
 
 rm -rf $1/recovery-from-boot.p
-# We have chrome for webview. so 100mb less size
-rm -rf $1/app/WebViewGoogle
 rm -rf $1/app/Drive
 rm -rf $1/app/Duo
 rm -rf $1/app/Maps
